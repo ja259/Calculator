8ec1ab3b229bf5f174d7cb3c63744f0f9387af4c
James Otwane <127046977+ja259@users.noreply.github.com>
2023-05-27 09:59:41 -0400

    Add javascript.js

--- javascript.js
+++ javascript.js
@@ -28,11 +28,27 @@ operators.forEach((op) => op.addEventListener("click", function(e){
 }))
 
 clear.addEventListener("click", function(){
-    previousValue = ' ';
-    currentValue = ' ';
-    operator = ' ';
+    previousValue = '';
+    currentValue = '';
+    operator = '';
     previousScreen.textContent = currentValue;
     currentScreen.textContent = currentValue;
+})
+
+equal.addEventListener("click", function(){
+    if(currentValue != '' && previousValue != ''){
+    calculate()
+    previousScreen.textContent = '';
+    if(previousValue.length <= 5){
+        currentScreen.textContent = previousValue;
+    } else{
+        currentScreen.textContent = previousValue.slice(0,5) + "...";
+    }
+}
+})
+
+decimal.addEventListener("click", function(){
+    addDecimal();
 })
 })
 
@@ -46,4 +62,33 @@ function handleOperator(op){
     operator = op;
     previousValue = currentValue;
     currentValue = '';
+}
+
+function calculate(){
+    previousValue = Number(previousValue);
+    currentValue = Number(currentValue);
+
+    if(operator === "+"){
+        previousValue += currentValue;
+    } else if(operator === "-"){
+        previousValue -= currentValue;
+    } else if(operator === "x"){
+        previousValue *= currentValue;
+    } else{
+        previousValue /= currentValue;
+    }
+
+    previousValue = roundNumber(previousValue);
+    previousValue = previousValue.toString();
+    currentValue = previousValue.toString();
+}
+
+function roundNumber(num){
+    return Math.round(num * 1000) / 1000;
+}
+
+function addDecimal(){
+    if(!currentValue.includes(".")){
+        currentValue += '.';
+    }
 }
